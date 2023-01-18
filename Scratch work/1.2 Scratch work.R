4d8517b0a48d6397863e06a6c91cce9f4a35fa98
yogigohel <yogi.gohel@rutgers.edu>
2023-01-18 14:54:52 -0500

    Updated homework and 1.2 pdf's. Added code and graphs to 1.2 notes.

--- Scratch work/1.2 Scratch work.R
+++ Scratch work/1.2 Scratch work.R
@@ -1,7 +1,8 @@
 library(ggplot2)
 library(ggfortify)
-
+library(astsa)
 set.seed(1)
+
 #(a)
 s_a=c(integer(100), 10*exp(-(1:100)/20)*cos(2*pi*1:100/4))
 x_a=s_a+rnorm(200)
@@ -18,4 +19,22 @@ mod_b=exp(-(1:100)/200)
 modulators<-as.ts(cbind(mod_a,mod_b))
 autoplot(modulators,facets=F,main="Modulators (a) and (b)")
          
-cbind()
+#Work for chapter notes
+##1.8
+w=rnorm(500)
+autoplot(as.ts(w), main="White Noise", xlab="t", ylab="wt", ylim=c(-4,4))
+##1.9
+v=filter(w, sides=2, filter=rep(1/3,3))
+autoplot(as.ts(v), main="Moving Average", xlab='t', ylab="yt", ylim=c(-4,4))
+##1.10
+w=rnorm(510)
+x=filter(w, filter=c(1,-.9), method = "recursive")[-(1:10)]
+autoplot(as.ts(x), main="Autoregression", xlab="t", ylab="xt")
+##1.11
+w=rnorm(200)
+d=0.2
+x=cumsum(w)
+wd=w+d
+xd=cumsum(wd)
+comparison=cbind(x,xd)
+autoplot(as.ts(comparison),facets=F,main="Random Walks")
